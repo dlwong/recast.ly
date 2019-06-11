59d3ab7efa2adc4208c87454a33f3fead7bcec82
dlwong <pingki@sbcglobal.net>
2019-06-10 21:43:49 -0700

    change videolistentry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,11 +3,10 @@ import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>Title</em> 
-    {props.videos.map((video)=> {
-      return <VideoListEntry videos = {video}/>
-    })}
-  </h5></div>
+    {props.videos.map(video => (
+      <VideoListEntry video={video} />
+    )
+    )}
   </div>
 );
 
