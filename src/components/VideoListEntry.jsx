a29bf9bf9b8d5efff23a483fabbf9d5e5ab1c912
dlwong <pingki@sbcglobal.net>
2019-06-10 19:15:23 -0700

    Implemented VideoList

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,20 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+
+
+var VideoListEntry = (props) => (
+          <div className="video-list-entry media">
+          <div className="media-left media-middle">
+            <img className="media-object" src={props.videos.snippet.thumbnails.default.url} alt="" />
+          </div>
+
+        <div className="media-body">
+          <div className="video-list-entry-title">{props.videos.snippet.title}</div>
+          <div className="video-list-entry-detail">{props.videos.snippet.description}</div>
+          </div>
+        </div>
+
+)
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
 
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
